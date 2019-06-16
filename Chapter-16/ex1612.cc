f0d83d0e99bde1d5fa4c5db20e7e2953b4215998
刘楚舒 <liuchushu10@gmail.com>
2019-06-16 17:14:35 +0800

    finished ex1612

---
+++ Chapter-16/ex1612.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename InputIterator>
+InputIterator FindMostFrequentValue(InputIterator first,
+                                    InputIterator last)
+{
+    InputIterator current = first;
+    int count = 0, max = 0;
+    InputIterator ret;
+
+    while (current != last) {
+        for (InputIterator iter = first; iter != last; ++iter) {
+            if (*current == *iter) {
+                ++count;
+            }
+        }
+        if (count > max) {
+            max = count;
+            ret = current;
+        }
+        ++current;
+    }
+    
+    return ret;
+}
+
+int main()
+{
+    static const int arr[] = {16,2,77,88,29,2,88,88};
+    vector<int> ivec(arr, arr + sizeof(arr) / sizeof(arr[0]) );
+    vector<int>::iterator it = FindMostFrequentValue(ivec.begin(), ivec.end() );
+    cout << *it << endl;
+    return 0;
+}
