90fa9bfef68b74e937cae6e12689a1933565fd9c
刘楚舒 <liuchushu10@gmail.com>
2019-06-18 22:55:47 +0800

    finished ex1615 ex1616

---
+++ Chapter-16/ex1615.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+template <class T, size_t N>
+size_t array_size(T (&parm)[N])
+{
+    if (parm == NULL) return 0;
+    return N;
+}
+
+int main()
+{
+    int arr[10];
+    double darr[55];
+    cout << array_size(arr) << endl;
+    cout << array_size(darr) << endl;
+    return 0;
+}
