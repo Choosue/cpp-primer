ad1160335495eac78d9e5738dbc30941b879bdfb
刘楚舒 <liuchushu10@gmail.com>
2019-06-23 22:15:02 +0800

    Test normal conversion

--- Chapter-16/compare_test.cc
+++ Chapter-16/compare_test.cc
@@ -9,10 +9,19 @@ int compare(const T &v1, const T &v2)
     return 0;
 }
 
+template <typename A, typename B>
+int compare_normal_conversion(const A &v1, const B &v2)
+{
+    if (v1 < v2) return -1;
+    if (v2 < v1) return 1;
+    return 0;
+}
+
 int main()
 {
     cout << compare(1, 2) << endl;
     // Compiler will not cast '1' automatically to double type
     // cout << compare(1, 2.2) << endl;
+    cout << compare_normal_conversion(10, 2.2) << endl;
     return 0;
 }
