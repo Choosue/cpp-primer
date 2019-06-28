14d4414689e651a2066279273b04badc708dcd1c
刘楚舒 <liuchushu10@gmail.com>
2019-06-28 23:32:24 +0800

    finished ex1625

---
+++ Chapter-16/ex1625.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+int compare(const T &v1, const T &v2)
+{
+    if (v1 < v2) return -1;
+    if (v2 < v1) return 1;
+    return 0;
+}
+
+int main()
+{
+    const string a = "Hello";
+    const string b = "World";
+    cout << compare<string>(a, b) << endl;
+    cout << compare<string>(b, a) << endl;
+    return 0;
+}
