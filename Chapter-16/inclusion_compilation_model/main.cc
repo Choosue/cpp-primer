58a2e98538dc3428312b5cdc5797e2de9fdfbba8
刘楚舒 <liuchushu10@gmail.com>
2019-07-02 23:23:23 +0800

    inclusion compilation model example

---
+++ Chapter-16/inclusion_compilation_model/main.cc
@@ -0,0 +1,10 @@
+#include "utilities.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 20;
+    cout << compare(a, b) << endl;
+    return 0;
+}
---
+++ Chapter-16/inclusion_compilation_model/utilities.h
@@ -0,0 +1,6 @@
+// header file utlities.h
+#ifndef UTLITIES_H // header gaurd (Section 2.9.2, p. 69)
+#define UTLITIES_H
+template <class T> int compare(const T&, const T&); // other declarations
+#include "utilities.cc" // get the definitions for compare etc.
+#endif
