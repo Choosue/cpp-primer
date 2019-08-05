fcdcebe3072e5ce66cc1fe6c2530a1cbf847fe05
刘楚舒 <liuchushu10@gmail.com>
2019-08-05 22:44:24 +0800

    refine ex1632

--- Chapter-16/ex1632.cc
+++ Chapter-16/ex1632.cc
@@ -2,9 +2,11 @@
 using namespace std;
 
 template <class Type> class Queue;
+template <class T> std::ostream& operator<<(std::ostream&, const Queue<T>&);
 
 template <class Type> class QueueItem {
     friend class Queue<Type>;
+    friend std::ostream& operator<< <Type> (std::ostream&, const Queue<Type>&);
 // private class: no public section
     QueueItem(const Type &t): item(t), next(0) { }
     Type item;       // value stored in this element
@@ -12,6 +14,7 @@ template <class Type> class QueueItem {
 };
 
 template <class Type> class Queue {
+    friend std::ostream& operator<< <Type> (std::ostream&, const Queue<Type>&);
 public:
     // empty Queue
     Queue(): head(0), tail(0) { }
@@ -26,7 +29,6 @@ public:
     void push(const Type &);
     void pop();
     bool empty() const { return head == 0; }
-    void display_elems();
 private:
     QueueItem<Type> *head;
     QueueItem<Type> *tail;
@@ -74,18 +76,22 @@ void Queue<Type>::copy_elems(const Queue &orig)
 }
 
 template <class Type>
+ostream& operator<<(ostream &os, const Queue<Type> &q)
+{
+    os << "< ";
+    QueueItem<Type> *p;
+    for (p = q.head; p; p = p->next) {
+        os << p->item << " ";
+    }
+    os << ">";
+    return os;
+}
+
+template <class Type>
 Queue<Type>& Queue<Type>::operator=(const Queue &orig)
 {
     copy_elems(orig);
     return *this;
-}
-
-template <class Type> void Queue<Type>::display_elems()
-{
-    for (QueueItem<Type> *pt = head; pt; pt = pt->next) {
-        cout << pt->item << " ";
-    }
-    cout << endl;
 }
 
 int main()
@@ -96,8 +102,8 @@ int main()
     queue.push(3);
     Queue<int> queue2(queue);
     Queue<int> queue3 = queue2;
-    queue.display_elems();
-    queue2.display_elems();
-    queue3.display_elems();
+    cout << queue << endl;
+    cout << queue2 << endl;
+    cout << queue3 << endl;
     return 0;
 }
